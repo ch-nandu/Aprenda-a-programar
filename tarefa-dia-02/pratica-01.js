9f27213dae488b343808a11372b3eb8b615c0bfc
Fernando <chnandu@gmail.com>
2022-08-26 11:29:57 -0300

    mensagem de commit 02

--- tarefa-dia-02/pratica-01.js
+++ tarefa-dia-02/pratica-01.js
@@ -1,7 +1,13 @@
 //Criando variaveis para a aula do segundo dia.
 
-let nome01
-let email01
-let profissao01
-let idade01
-let desejo01
+let nome01 = "Nandu"
+let email01 = "nandu@gmail.com"
+let profissao01 = "Eletricista"
+let idade01 = 35
+let desejo01 = "Caribe"
+
+console.log(nome01)
+console.log(email01)
+console.log(profissao01)
+console.log(idade01)
+console.log(desejo01)
