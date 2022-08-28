db7a252d8768de7784ba6f3f43db7290a41bfbbc
Fernando <chnandu@gmail.com>
2022-08-28 20:16:11 -0300

    mensagem commit 006

---
+++ tarefa-dia-03/praticando-02.js
@@ -0,0 +1,10 @@
+//Tabuada
+
+let multiplicador = 9
+let n = 10 // Vou multiplicar de 1 até "N" 
+let i = 1
+ 
+while (i <= n) {
+ console.log(`${multiplicador} X ${i} = ${multiplicador * i}`)
+i++
+}
