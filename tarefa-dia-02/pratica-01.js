f426465fc720d741988fcca507d2bb171b154df5
Fernando <chnandu@gmail.com>
2022-08-28 09:22:55 -0300

    mensagem de commit 03

--- tarefa-dia-02/pratica-01.js
+++ tarefa-dia-02/pratica-01.js
@@ -1,11 +1,16 @@
 //Criando variaveis para a aula do segundo dia.
 
 let nome01 = "Nandu"
-let email01 = "nandu@gmail.com"
+let email01 = "nandu@aol.com"
 let profissao01 = "Eletricista"
 let idade01 = 35
 let desejo01 = "Caribe"
 
+let mensagem = "Olá, " + nome01 + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + desejo01 + ". Confirma pra gente alguns dados? A sua idade é " + idade01 + ", você é " + profissao01 +" e o seu email é " + email01 + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D" 
+
+
+
+console.log(mensagem)
 console.log(nome01)
 console.log(email01)
 console.log(profissao01)
