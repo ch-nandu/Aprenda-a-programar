447f673662b8fdc794e0e5818e55f3321af08840
Fernando <chnandu@gmail.com>
2022-08-28 21:34:54 -0300

    Mensagem commit 007

---
+++ tarefa-dia-03/praticando-03.js
@@ -0,0 +1,13 @@
+// Exibe os numero Impares no intervalo de um valor "N"
+
+let n = 16 // Até esse valor
+let i = 1
+let resto = 0
+
+while(i <= n){
+if ( i % 2 == 1) {
+    console.log(i)
+    
+}  
+i++
+}
